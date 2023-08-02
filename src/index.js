97ab4b7ba4a4bcbf977c9b94ec36a3ece359a122
Gasay-Liza <gasayliz@yandex.ru>
2023-08-02 15:44:51 +0300

    Разработка декстопной версии первого слайда

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// const header = document.querySelector('.header');
+// const burger = header.querySelector('.burger');
+// const navItem = header.querySelector('.navigation__item');
+// const telephone= header.querySelector('.header__contact');
+
+// // Функция для отображения или скрытия навигации и изменения состояния бургер-меню
+// function toggleNav() {
+//     console.log(burger)
+//     navItem.classList.toggle('active');
+//     burger.classList.toggle('active');
+//     telephone.classList.toggle('active');
+//   }
+//   console.log(burger)
+//   // Переключение состояния навигации и бургер-меню при клике на бургер
+//   burger.addEventListener('click', toggleNav);

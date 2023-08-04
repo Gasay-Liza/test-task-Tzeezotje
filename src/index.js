3e36e7f449cc26261f2b2932ec67e1174f5401b8
Gasay-Liza <gasayliz@yandex.ru>
2023-08-04 22:48:53 +0300

    Разработка бургер-меню

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 const burger = header.querySelector('.burger');
-const navItem = header.querySelector('.navigation__item');
-const telephone= header.querySelector('.header__contact');
+const navigation = header.querySelector('.navigation');
+const navList = navigation.querySelector('.navigation__list');
+const navItem = navigation.querySelector('.navigation__item');
+const telephone= navigation.querySelector('.header__contact');
 
 // Функция для отображения или скрытия навигации и изменения состояния бургер-меню
 function toggleNav() {
     console.log(burger)
-    navItem.classList.toggle('active');
+    navigation.classList.toggle('active');
+    overlay.classList.toggle('active');
     burger.classList.toggle('active');
-    telephone.classList.toggle('active');
+  }
+
+  function handleClose() {
+    console.log(burger)
+    navigation.classList.remove('active');
+    overlay.classList.remove('active');
+    burger.classList.remove('active');
   }
   console.log(burger)
   // Переключение состояния навигации и бургер-меню при клике на бургер
   burger.addEventListener('click', toggleNav);
-
+  overlay.addEventListener('click', toggleNav);
+  document.addEventListener('keydown', function (e) {
+    if(e.key === "Escape") {
+      handleClose()
+    }
+  }); 
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
